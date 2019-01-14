47f479d4da0b56d8174f873ad13251f28efcee09
Bob Hunt <bob.hunt.email@gmail.com>
2019-01-14 13:00:21 -0500

    Cleaned up code. Attempted to resolve conflicts with davidstewy's code.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from itertools import permutations
 """
 Molecules Assessment
 """
-_author_ = "bomazani, Stew, with mob-coding assistance"
 
-from itertools import permutations
+_author_ = "bomazani, Stew, with mob-coding assistance"
 
 
 molecules_to_check = ['CDBADCBBEFEF', 'DACCBADAFEAB',
@@ -23,21 +24,8 @@ molecules_to_check = ['CDBADCBBEFEF', 'DACCBADAFEAB',
                       'Q']
 
 
-def rectangle_tuples(molecules_to_check):
-    # Generates list of (h, W) tuples. Runs once.
-    print('Getting list of rectangle tuples')
-    str_length = len(molecules_to_check[0])
-    pairs = [(w, h) for w in range(2, (str_length - 1))
-             for h in range(w, (str_length - 1))]
-    pairs.sort(
-        key=lambda str_length: str_length[0] * str_length[1], reverse=True)
-    print('Pairs = {}'.format(pairs))
-    return pairs
-
-
 def grab_molecule(molecules_to_check):
     # Pulls molecule (4 strings) from the list of strings.
-    # Runs until all strings have been checked.
     print('Grabbing molecule')
     molecule_list = []
     molecule = []
@@ -52,9 +40,20 @@ def grab_molecule(molecules_to_check):
         molecule_list.append(molecule)
 
 
+def rectangle_tuples(molecules_to_check):
+    # Generates list of (h, W) tuples. Runs once.
+    str_length = len(molecules_to_check[0])
+    pairs = [(w, h) for w in range(2, (str_length - 1))
+             for h in range(w, (str_length - 1))]
+    pairs.sort(
+        key=lambda str_length: str_length[0] * str_length[1], reverse=True)
+    return pairs
+
+
 def best_fit(w, h, across1, down1, across2, down2):
-    # Tries to fit a set of 4 molecule strings around a rectangle.
-    # Returns true if a fit is found.
+    """ Tries to fit a set of 4 molecule strings around a rectangle.
+     Returns true if a fit is found.
+     """
     for a1 in range(1, 12 - (w + 1)):
         for d1 in range(1, 12 - (h + 1)):
             if across1[a1] != down1[d1]:
@@ -66,38 +65,35 @@ def best_fit(w, h, across1, down1, across2, down2):
                     if down2[d2] != across1[a1 + (w + 1)]:
                         continue
                     if down2[d2 + (h + 1)] == across2[a2 + (w + 1)]:
-                        print('Found a match!')
                         return True
-    print('No match')
     return False
 
 
 def main():
-
-    # Call rectangle_tuples
+    molecules_strings = grab_molecule(molecules_to_check)
     rectangle_dimensions = rectangle_tuples(molecules_to_check)
-    print('rectangle_dimensions = {}'.format(rectangle_dimensions))
-
-    # Call grap_molecule
-    molecule_strings = grab_molecule(molecules_to_check)
-    print('molecule strings = {}'.format(molecule_strings))
-
-    molecules_variations = list(permutations(molecule_strings))
-    print('molecules_variations = {}'.format(molecules_variations))
-    for w, x, y, z in molecules_variations:
-        print('w,x,y,z = {},{},{},{}'.format(w, x, y, z))
+    for molecule_strings in molecules_strings:
+        answer = 0
+        molecules_variations = list(permutations(molecule_strings))
+        print('molecules_variations = {}'.format(molecules_variations))
 
         for w, h in rectangle_dimensions:
             print('Rectangle: w = {}, h={}'.format(w, h))
-
+            if answer == 1:
+                break
             for molecule in molecules_variations:
                 print('molecule = {}'.format(molecule))
-
                 if best_fit(w, h, *molecule):
+                    print(molecule_strings)
                     print('area = {}'.format(w * h))
+                    answer = 1
+                    break
                 else:
                     print('continuing')
                     continue
+        if answer == 0:
+            print(molecule_strings)
+            print('0')
 
 
 # across1, down1, across2, down2
